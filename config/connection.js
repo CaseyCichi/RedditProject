3c9cd550351354b7a7078b2d9b89e978e8c2a3a6
Gus Nguyen <nguyenhachuy@gmail.com>
2017-12-31 16:22:58 -0800

    Added materialize with new layout for better design and scalability

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,10 @@ var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "",
+  password: "11051997",
   database: "cat_db"
 });
 
-// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -18,5 +17,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
